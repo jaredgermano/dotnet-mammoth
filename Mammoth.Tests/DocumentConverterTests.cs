0dac955e7cf14429cb85dfc85dd129cc2ea4c324
Jared Germano <jgermano@cadlearning.com>
2018-12-13 07:19:13 -0500

    Dotnet standard

--- Mammoth.Tests/DocumentConverterTests.cs
+++ Mammoth.Tests/DocumentConverterTests.cs
@@ -5,45 +5,53 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Mammoth.Tests {
-    public class DocumentConverterTests {
+namespace Mammoth.Tests
+{
+    public class DocumentConverterTests
+    {
         [Fact]
-        public void DocxContainingOneParagraphIsConvertedToSingleParagraphElement() {
+        public void DocxContainingOneParagraphIsConvertedToSingleParagraphElement()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("single-paragraph.docx"),
                 "<p>Walking on imported air</p>");
         }
 
         [Fact]
-        public void CanReadFilesWithUtf8Bom() {
+        public void CanReadFilesWithUtf8Bom()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("utf8-bom.docx"),
                 "<p>This XML has a byte order mark.</p>");
         }
 
         [Fact]
-        public void EmptyParagraphsAreIgnoredByDefault() {
+        public void EmptyParagraphsAreIgnoredByDefault()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("empty.docx"),
                 "");
         }
 
         [Fact]
-        public void EmptyParagraphsArePreservedIfIgnoreEmptyParagraphsIsFalse() {
+        public void EmptyParagraphsArePreservedIfIgnoreEmptyParagraphsIsFalse()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("empty.docx", converter => converter.PreserveEmptyParagraphs()),
                 "<p></p>");
         }
 
         [Fact]
-        public void SimpleListIsConvertedToListElements()  {
+        public void SimpleListIsConvertedToListElements()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("simple-list.docx"),
                 "<ul><li>Apple</li><li>Banana</li></ul>");
         }
 
         [Fact]
-        public void WordTablesAreConvertedToHtmlTables() {
+        public void WordTablesAreConvertedToHtmlTables()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("tables.docx"),
                 "<p>Above</p>" +
@@ -55,81 +63,96 @@ namespace Mammoth.Tests {
         }
 
         [Fact]
-        public void InlineImagesReferencedByPathRelativeToPartAreIncludedInOutput() {
+        public void InlineImagesReferencedByPathRelativeToPartAreIncludedInOutput()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("tiny-picture.docx"),
                 "<p><img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAAXNSR0IArs4c6QAAAAlwSFlzAAAOvgAADr4B6kKxwAAAABNJREFUKFNj/M+ADzDhlWUYqdIAQSwBE8U+X40AAAAASUVORK5CYII=\" /></p>");
         }
 
         [Fact]
-        public void InlineImagesReferencedByPathRelativeToBaseAreIncludedInOutput() {
+        public void InlineImagesReferencedByPathRelativeToBaseAreIncludedInOutput()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("tiny-picture-target-base-relative.docx"),
                 "<p><img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAAXNSR0IArs4c6QAAAAlwSFlzAAAOvgAADr4B6kKxwAAAABNJREFUKFNj/M+ADzDhlWUYqdIAQSwBE8U+X40AAAAASUVORK5CYII=\" /></p>");
         }
 
         [Fact]
-        public void ImagesStoredOutsideOfDocumentAreIncludedInOutput() {
+        public void ImagesStoredOutsideOfDocumentAreIncludedInOutput()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("external-picture.docx"),
                 "<p><img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAAXNSR0IArs4c6QAAAAlwSFlzAAAOvgAADr4B6kKxwAAAABNJREFUKFNj/M+ADzDhlWUYqdIAQSwBE8U+X40AAAAASUVORK5CYII=\" /></p>");
         }
 
         [Fact]
-        public void WarnIfDocumentHasImagesStoredOutsideOfDocumentWhenPathOfDocumentIsUnknown() {
-            using (var file = File.OpenRead(TestFilePath("external-picture.docx"))) {
+        public void WarnIfDocumentHasImagesStoredOutsideOfDocumentWhenPathOfDocumentIsUnknown()
+        {
+            using (var file = File.OpenRead(TestFilePath("external-picture.docx")))
+            {
                 var result = new DocumentConverter().ConvertToHtml(file);
                 Assert.Equal("", result.Value);
-                Assert.Equal(new[]{"could not open external image 'tiny-picture.png': path of document is unknown, but is required for relative URI"}, result.Warnings);
+                Assert.Equal(new[] { "could not open external image 'tiny-picture.png': path of document is unknown, but is required for relative URI" }, result.Warnings);
             }
         }
 
         [Fact]
-        public void WarnIfImagesStoredOutsideOfDocumentAreNotFound() {
+        public void WarnIfImagesStoredOutsideOfDocumentAreNotFound()
+        {
             var tempDirectory = Path.Combine(Path.GetTempPath(), "mammoth-" + Guid.NewGuid());
             Directory.CreateDirectory(tempDirectory);
-            try {
+            try
+            {
                 var documentPath = Path.Combine(tempDirectory, "external-picture.docx");
                 File.Copy(TestFilePath("external-picture.docx"), documentPath);
                 var result = new DocumentConverter().ConvertToHtml(documentPath);
                 Assert.Equal("", result.Value);
                 Assert.StartsWith("could not open external image 'tiny-picture.png':", result.Warnings.Single());
-            } finally {
+            }
+            finally
+            {
                 Directory.Delete(tempDirectory, recursive: true);
             }
         }
-        
+
         [Fact]
-        public void ImageConversionCanBeCustomised() {
+        public void ImageConversionCanBeCustomised()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("tiny-picture.docx", mammoth => mammoth.ImageConverter(ConvertImage)),
                 "<p><img src=\"iV,image/png\" /></p>"
             );
         }
-        
-        private IDictionary<string, string> ConvertImage(IImage image) {
-            using (var stream = image.GetStream()) {
+
+        private IDictionary<string, string> ConvertImage(IImage image)
+        {
+            using (var stream = image.GetStream())
+            {
                 var base64 = StreamToBase64(stream);
                 var src = base64.Substring(0, 2) + "," + image.ContentType;
                 return new Dictionary<string, string> { { "src", src } };
             }
         }
-        
-        private static string StreamToBase64(System.IO.Stream stream) {
+
+        private static string StreamToBase64(System.IO.Stream stream)
+        {
             var memoryStream = new System.IO.MemoryStream();
             stream.CopyTo(memoryStream);
             return System.Convert.ToBase64String(memoryStream.ToArray());
         }
 
         [Fact]
-        public void ContentTypesAreRead() {
+        public void ContentTypesAreRead()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("tiny-picture-custom-content-type.docx"),
                 "<p><img src=\"data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAAXNSR0IArs4c6QAAAAlwSFlzAAAOvgAADr4B6kKxwAAAABNJREFUKFNj/M+ADzDhlWUYqdIAQSwBE8U+X40AAAAASUVORK5CYII=\" /></p>");
         }
 
         [Fact]
-        public void FootnotesAreAppendedToText() {
+        public void FootnotesAreAppendedToText()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("footnotes.docx", mammoth => mammoth.IdPrefix("doc-42-")),
                     "<p>Ouch" +
@@ -140,7 +163,8 @@ namespace Mammoth.Tests {
         }
 
         [Fact]
-        public void EndNotesAreAppendedToText() {
+        public void EndNotesAreAppendedToText()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("endnotes.docx", mammoth => mammoth.IdPrefix("doc-42-")),
                     "<p>Ouch" +
@@ -151,7 +175,8 @@ namespace Mammoth.Tests {
         }
 
         [Fact]
-        public void RelationshipsAreReadForEachFileContainingBodyXml() {
+        public void RelationshipsAreReadForEachFileContainingBodyXml()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("footnote-hyperlink.docx", mammoth => mammoth.IdPrefix("doc-42-")),
 
@@ -160,53 +185,60 @@ namespace Mammoth.Tests {
         }
 
         [Fact]
-        public void TextBoxesAreRead() {
+        public void TextBoxesAreRead()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("text-box.docx"),
                 "<p>Datum plane</p>");
         }
 
         [Fact]
-        public void CanUseCustomStyleMap() {
+        public void CanUseCustomStyleMap()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("underline.docx", mammoth => mammoth.AddStyleMap("u => em")),
                 "<p><strong>The <em>Sunset</em> Tree</strong></p>");
         }
 
         [Fact]
-        public void MostRecentlyAddedStyleMapTakesPrecedence() {
+        public void MostRecentlyAddedStyleMapTakesPrecedence()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("underline.docx", mammoth => mammoth.AddStyleMap("u => em").AddStyleMap("u => strong")),
                 "<p><strong>The <strong>Sunset</strong> Tree</strong></p>");
         }
 
         [Fact]
-        public void RulesFromPreviouslyAddedStyleMapsStillTakeEffectIfNotOverridden() {
+        public void RulesFromPreviouslyAddedStyleMapsStillTakeEffectIfNotOverridden()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("underline.docx", mammoth => mammoth.AddStyleMap("u => em").AddStyleMap("strike => del")),
                 "<p><strong>The <em>Sunset</em> Tree</strong></p>");
         }
 
         [Fact]
-        public void ErrorIsRaisedIfStyleMapCannotBeParsed() {
-	    var exception = Assert.ThrowsAny<Exception>(() =>
-		new DocumentConverter().AddStyleMap("p =>\np[style-name=] =>"));
-	    Assert.Equal(
-		"error reading style map at line 2, character 14: expected token of type _STRING but was of type _SYMBOL\n\n" +
-                "p[style-name=] =>\n" +
-                "             ^",
-		exception.Message);
+        public void ErrorIsRaisedIfStyleMapCannotBeParsed()
+        {
+            var exception = Assert.ThrowsAny<Exception>(() =>
+            new DocumentConverter().AddStyleMap("p =>\np[style-name=] =>"));
+            Assert.Equal(
+            "error reading style map at line 2, character 14: expected token of type _STRING but was of type _SYMBOL\n\n" +
+                    "p[style-name=] =>\n" +
+                    "             ^",
+            exception.Message);
         }
 
         [Fact]
-        public void CanDisableDefaultStyleMap() {
+        public void CanDisableDefaultStyleMap()
+        {
             var result = ConvertToHtml("simple-list.docx", mammoth => mammoth.DisableDefaultStyleMap());
-            Assert.Equal(new[]{"Unrecognised paragraph style: List Paragraph (Style ID: ListParagraph)"}, result.Warnings);
+            Assert.Equal(new[] { "Unrecognised paragraph style: List Paragraph (Style ID: ListParagraph)" }, result.Warnings);
             Assert.Equal("<p>Apple</p><p>Banana</p>", result.Value);
         }
 
         [Fact]
-        public void EmbeddedStyleMapIsUsedIfPresent() {
+        public void EmbeddedStyleMapIsUsedIfPresent()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("embedded-style-map.docx"),
                 "<h1>Walking on imported air</h1>"
@@ -214,7 +246,8 @@ namespace Mammoth.Tests {
         }
 
         [Fact]
-        public void ExplicitStyleMapTakesPrecedenceOverEmbeddedStyleMap() {
+        public void ExplicitStyleMapTakesPrecedenceOverEmbeddedStyleMap()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("embedded-style-map.docx", mammoth => mammoth.AddStyleMap("p => p")),
                 "<p>Walking on imported air</p>"
@@ -222,7 +255,8 @@ namespace Mammoth.Tests {
         }
 
         [Fact]
-        public void ExplicitStyleMapIsCombinedWithEmbeddedStyleMap() {
+        public void ExplicitStyleMapIsCombinedWithEmbeddedStyleMap()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("embedded-style-map.docx", mammoth => mammoth.AddStyleMap("r => strong")),
                 "<h1><strong>Walking on imported air</strong></h1>"
@@ -230,7 +264,8 @@ namespace Mammoth.Tests {
         }
 
         [Fact]
-        public void embeddedStyleMapsCanBeDisabled() {
+        public void embeddedStyleMapsCanBeDisabled()
+        {
             AssertSuccessfulConversion(
                 ConvertToHtml("embedded-style-map.docx", mammoth => mammoth.DisableEmbeddedStyleMap()),
                 "<p>Walking on imported air</p>"
@@ -238,38 +273,46 @@ namespace Mammoth.Tests {
         }
 
         [Fact]
-        public void CanExtractRawTextFromFile() {
+        public void CanExtractRawTextFromFile()
+        {
             AssertSuccessfulConversion(
                 new DocumentConverter().ExtractRawText(TestFilePath("simple-list.docx")),
                 "Apple\n\nBanana\n\n");
         }
 
         [Fact]
-        public void CanExtractRawTextFromStream() {
-            using (var file = File.OpenRead(TestFilePath("simple-list.docx"))) {
+        public void CanExtractRawTextFromStream()
+        {
+            using (var file = File.OpenRead(TestFilePath("simple-list.docx")))
+            {
                 AssertSuccessfulConversion(
                     new DocumentConverter().ExtractRawText(file),
                     "Apple\n\nBanana\n\n");
             }
         }
 
-        private void AssertSuccessfulConversion(IResult<string> result, string expectedValue) {
-            if (result.Warnings.Count > 0) {
+        private void AssertSuccessfulConversion(IResult<string> result, string expectedValue)
+        {
+            if (result.Warnings.Count > 0)
+            {
                 throw new XunitException("Unexpected warnings: " + string.Join(", ", result.Warnings));
             }
             Assert.Equal(expectedValue, result.Value);
         }
 
-        private IResult<string> ConvertToHtml(string name) {
+        private IResult<string> ConvertToHtml(string name)
+        {
             return ConvertToHtml(name, converter => converter);
         }
 
-        private IResult<string> ConvertToHtml(string name, Func<DocumentConverter, DocumentConverter> configure) {
+        private IResult<string> ConvertToHtml(string name, Func<DocumentConverter, DocumentConverter> configure)
+        {
             return configure(new DocumentConverter()).ConvertToHtml(TestFilePath(name));
         }
 
-        private string TestFilePath(string name) {
-            return Path.Combine("../../TestData", name);
+        private string TestFilePath(string name)
+        {
+            return Path.Combine("../../../TestData", name);
         }
     }
 }
